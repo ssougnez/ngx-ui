4cae8204fe71d24007c8bafbe60c9f2a4b66e27e
ssougnez <s.sougnez@areaprog.com>
2021-08-04 21:24:05 +0200

    Now... The tests...

--- projects/ngx-ui/src/lib/components/accordion/accordion.component.ts
+++ projects/ngx-ui/src/lib/components/accordion/accordion.component.ts
@@ -1,22 +1,16 @@
 import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, ContentChildren, Directive, ElementRef, EventEmitter, forwardRef, Input, OnInit, Output, QueryList, TemplateRef, ViewEncapsulation } from '@angular/core';
 import { ColorScheme, IconBrush } from '../../common';
+import { FocusPosition } from '../../internal/enumerations';
 
 type ItemState = 'expanded' | 'collapsed';
 type StateBehavior = 'single' | 'multiple';
-
-let nextUniqueId = 0;
-
-enum FocusPosition {
-  First,
-  Next,
-  Previous,
-  Last
-}
 
 export type AccordionItemStateChangedEventData = {
   item: AccordionItemComponent;
   index: number;
 }
+
+let nextUniqueId = 0;
 
 @Directive({
   selector: '[ngx-ui-accordion-item-title]'
@@ -42,10 +36,10 @@ export class AccordionItemContentDirective {
   styleUrls: ['./accordion.component.scss'],
   host: {
     '[class]': '"ngx-ui-accordion-" + (color || "primary")',
-    '(keydown.home)': 'moveFocus(FocusPosition.First, $event)',
-    '(keydown.end)': 'moveFocus(FocusPosition.Last, $event)',
-    '(keydown.arrowdown)': 'moveFocus(FocusPosition.Next, $event)',
-    '(keydown.arrowup)': 'moveFocus(FocusPosition.Previous, $event)'
+    '(keydown.home)': '_moveFocus(_FocusPosition.First, $event)',
+    '(keydown.end)': '_moveFocus(_FocusPosition.Last, $event)',
+    '(keydown.arrowdown)': '_moveFocus(_FocusPosition.Next, $event)',
+    '(keydown.arrowup)': '_moveFocus(_FocusPosition.Previous, $event)'
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
@@ -77,7 +71,7 @@ export class AccordionComponent implements AfterContentInit {
 
   /****************************************************************** ENUMERATIONS ******************************************************************/
 
-  public FocusPosition: typeof FocusPosition = FocusPosition;
+  public _FocusPosition: typeof FocusPosition = FocusPosition;
 
   /****************************************************************** LIFE CYCLE ******************************************************************/
 
@@ -85,44 +79,48 @@ export class AccordionComponent implements AfterContentInit {
   public ngAfterContentInit() {
     if (this.behavior === 'single') {
       this.children
-        .filter(c => c.state === 'expanded')
+        .filter(c => c._state === 'expanded')
         .slice(1)
-        .forEach(c => c.updateState('collapsed'));
+        .forEach(c => c._updateState('collapsed'));
     }
   }
 
-  /****************************************************************** PUBLIC ******************************************************************/
+  /****************************************************************** INTERNAL ******************************************************************/
 
   /**
    * 
    * @param trigger
    */
-  public ensureState(trigger: AccordionItemComponent) {
+  public _ensureState(trigger: AccordionItemComponent) {
     if (this.behavior === 'single') {
       this.children
         .filter(c => c !== trigger)
-        .forEach(c => c.updateState('collapsed'));
+        .forEach(c => c._updateState('collapsed'));
     }
   }
 
-  /** */
-  public moveFocus(position: FocusPosition, event: KeyboardEvent) {
+  /**
+   * 
+   * @param position
+   * @param event
+   */
+  public _moveFocus(position: FocusPosition, event: KeyboardEvent) {
     if (this.children.length !== 0) {
       const children = this.children.toArray();
-      const focusedIndex = children.findIndex(c => c.hasFocus === true);
+      const focusedIndex = children.findIndex(c => c._hasFocus === true);
       const focused = children[focusedIndex];
 
       if (position === FocusPosition.First) {
-        this.children.first.focus();
+        this.children.first._focus();
       }
       else if (position === FocusPosition.Next && focused !== this.children.last) {
-        children[focusedIndex + 1].focus();
+        children[focusedIndex + 1]._focus();
       }
       else if (position === FocusPosition.Previous && focused !== this.children.first) {
-        children[focusedIndex - 1].focus();
+        children[focusedIndex - 1]._focus();
       }
       else if (position === FocusPosition.Last) {
-        this.children.last.focus();
+        this.children.last._focus();
       }
 
       event.preventDefault();
@@ -133,8 +131,8 @@ export class AccordionComponent implements AfterContentInit {
    * 
    * @param item
    */
-  public propagateChange(item: AccordionItemComponent) {
-    this.itemStateChanged.emit({ item: item, index: this.children.toArray().findIndex(c => c === item) });
+  public _propagateChange(item: AccordionItemComponent) {
+    this.itemStateChanged.emit({ item, index: this.children.toArray().findIndex(c => c === item) });
   }
 }
 
@@ -169,8 +167,8 @@ export class AccordionItemComponent implements OnInit {
 
   /****************************************************************** VARIABLES ******************************************************************/
 
-  public hasFocus = false;
-  public state: ItemState = 'expanded';
+  public _hasFocus = false;
+  public _state: ItemState = 'expanded';
 
   /****************************************************************** LIFE CYCLE ******************************************************************/
 
@@ -178,13 +176,13 @@ export class AccordionItemComponent implements OnInit {
 
   /** */
   public ngOnInit() {
-    this.state = this.initialState || 'expanded';
+    this._state = this.initialState || 'expanded';
   }
 
-  /****************************************************************** PUBLIC ******************************************************************/
+  /****************************************************************** INTERNAL ******************************************************************/
 
   /** */
-  public focus() {
+  public _focus() {
     this._elementRef.nativeElement.firstChild.focus();
   }
 
@@ -192,23 +190,21 @@ export class AccordionItemComponent implements OnInit {
    * 
    * @param hasFocus
    */
-  public focusChanged(hasFocus: boolean) {
-    this.hasFocus = hasFocus;
+  public _focusChanged(hasFocus: boolean) {
+    this._hasFocus = hasFocus;
   }
 
   /**
    * 
    * @param state
    */
-  public updateState(state: ItemState) {
-    if (this.state !== state) {
-      this.state = state;
+  public _updateState(state: ItemState) {
+    if (this._state !== state) {
+      this._state = state;
 
-      if (state === 'expanded') {
-        this.parent.ensureState(this);
-      }
+      state === 'expanded' && this.parent._ensureState(this);
 
-      this.parent.propagateChange(this);
+      this.parent._propagateChange(this);
 
       this._cdr.markForCheck();
     }
---
+++ projects/ngx-ui/src/lib/internal/enumerations/focus-position.ts
@@ -0,0 +1,6 @@
+export enum FocusPosition {
+  First,
+  Next,
+  Previous,
+  Last
+}
